7908b6fb16c70df4ac28e685cc9d1ca6bf694547
= <=>
2025-10-30 10:17:19 -0300

    Add types files

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import env from "../env.js";
-import * as entitySchema from "./schema/index";
+import * as entitySchema from "./schema/index.js";
 
 export const database = drizzle(env.DATABASE_URL, {
   schema: entitySchema,
